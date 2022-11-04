60bdde6e2df6abab2bc5ed78e06466f594920d31
JuanKitu <juanmascruz@gmail.com>
2022-11-03 22:41:56 -0300

    newFeat - feat: files that are not part of the solution were deleted

--- src/modules/example/example2.get.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { sendSuccess } from '../../core/traffic.core';
-
-export default async function example1(req: Request, res: Response) {
-  const { id } = req.params;
-  return sendSuccess(res, 'ok', { message: 'hi', id });
-}
