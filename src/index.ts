177b214e22e207dc828094a1162ff176ccbc461d
JuanKitu <juanmascruz@gmail.com>
2022-11-03 23:07:00 -0300

    newFeat - fix: added .then to sequelize

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import sequelize from './database/database';
 import app from './app';
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-sequelize.sync();
+sequelize.sync().then();
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
   console.log(clc.magenta.inverse.bold(`Server on: localhost:${app.get('port')}`));
