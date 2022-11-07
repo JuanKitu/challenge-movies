ad3798a081db1fd13fdd34daf2383269ef8f8a2c
JuanKitu <juanmascruz@gmail.com>
2022-11-07 10:31:09 -0300

    newFeat - feat: added filter duration

--- src/modules/movies/movies.get.spec.ts
+++ src/modules/movies/movies.get.spec.ts
@@ -155,6 +155,41 @@ describe('GET movies', () => {
     };
     expect(mockResponse.send).toBeCalledWith(expectedResponse);
   });
+  test('returns ok a simple filter and order valid', async () => {
+    mockRequest = { query: { filter: 'duration:01:22:00', orderby: 'name:te' } };
+    await moviesModule(mockRequest as Request, mockResponse as Response);
+    const birthdate = new Date('10/10/2020');
+    const releaseDate = new Date('10/22/1996');
+    const expectedResponse = {
+      code: 201,
+      error: false,
+      message: 'ok',
+      payload: {
+        moviesList: [
+          {
+            actors: [],
+            budget: '15 millions',
+            director: 1,
+            directors: {
+              artist: 1,
+              birthdate,
+              firstName: 'test',
+              gender: 'Masculine',
+              height: 1.67,
+              lastName: 'test2',
+            },
+            duration: '01:22:00',
+            gender: 'drama',
+            languages: 'English',
+            movie: 1,
+            name: 'test movie',
+            releaseDate,
+          },
+        ],
+      },
+    };
+    expect(mockResponse.send).toBeCalledWith(expectedResponse);
+  });
   test('returns a complex filter and order valid', async () => {
     mockRequest = { query: { filter: ['movie:1', 'name:te'], orderby: ['gender:DESC', 'movie:ASC'] } };
     await moviesModule(mockRequest as Request, mockResponse as Response);
